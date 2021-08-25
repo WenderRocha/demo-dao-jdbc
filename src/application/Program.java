36323716ca341c09d18420f3efc69fb7fb4aa11e
Wender Rocha <wender_dev@hotmail.com>
2021-08-24 22:15:22 -0400

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,16 @@
 package application;
 
+import java.sql.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Seller seller = new Seller(21, "Wender", "wender_dev@hotmail.com", new Date(0), 3000.0, obj);
+		System.out.println(seller);
 
 	}
 
